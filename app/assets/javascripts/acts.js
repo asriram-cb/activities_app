1c52782ca524961c2de368fd23d893ede5d78b98
solostyle <spam@solostyle.net>
2014-02-27 16:56:22 -0600

    styled the buttons

--- app/assets/javascripts/acts.js
+++ app/assets/javascripts/acts.js
@@ -4,6 +4,10 @@
   var bindActCreators = function () {
     $(".act-creator").click(function (e) {
       e.preventDefault();
+      $(".act-creator").each(function () {
+        $(this).removeClass("btn-success").addClass("btn-default");
+      });
+      $(this).removeClass("btn-default").addClass("btn-success");
       var arr = $(this).attr('id').split('-');
       $("#act_activity_id").val(arr[arr.length-1]);
     });
