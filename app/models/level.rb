5a52f6e9f1baccc95a1629105e89d9b7a5f095a3
solostyle <spam@solostyle.net>
2014-03-05 15:54:38 -0600

    added tests for level

--- app/models/level.rb
+++ app/models/level.rb
@@ -3,7 +3,7 @@ class Level < ActiveRecord::Base
   # relationships
   # validations
   validates :title, presence: true, length: { minimum: 3, maximum: 20 }
-  validates :number, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :number, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }, uniqueness: true
   validates :calorie_goal, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validates :activity_goal, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 end
---
+++ spec/models/level_spec.rb
@@ -0,0 +1,110 @@
+require 'spec_helper'
+
+describe Level do
+  let(:user) { FactoryGirl.create(:complete_user) }
+
+  before do
+    # give this user several activities?
+    # at a certain level? 
+    @level = Level.first
+    @new_level = Level.create(title: "Mega Awesome", calorie_goal: 50000, activity_goal: 60, number: 5)
+  end
+
+  subject { @level }
+
+  it { should respond_to(:title) }
+  it { should respond_to(:number) }
+  it { should respond_to(:calorie_goal) }
+  it { should respond_to(:activity_goal) }
+  it { should be_valid } # it's already inserted, so isn't it valid?
+
+  subject { @new_level }
+  it { should respond_to(:title) }
+  it { should respond_to(:number) }
+  it { should respond_to(:calorie_goal) }
+  it { should respond_to(:activity_goal) }
+  it { should be_valid }
+
+  describe "when title is not present" do
+    before { @new_level.title = " " }
+    it { should_not be_valid }
+  end
+
+  describe "when number is not unique" do
+    before do
+      Level.create!(title: "Mega Awesome", calorie_goal: 50000, activity_goal: 60, number: 5)
+      @new_level.save
+    end
+    it { should_not be_valid }
+  end
+
+  describe "when title is fewer than three characters long" do
+    before { @new_level.title = "ab" }
+    it { should_not be_valid }
+  end
+
+  describe "when title is greater than 20 characters long" do
+    before { @new_level.name = "a" * 21 }
+    it { should_not be_valid }
+  end
+
+  describe "when number is not present" do
+    before { @new_level.number = " " }
+    it { should_not be_valid }
+  end
+
+  describe "when number is negative" do
+    before { @new_level.number = -1 }
+    it { should_not be_valid }
+  end
+
+  describe "when number is not numeric" do
+    before { @new_level.number = "five" }
+    it { should_not be_valid }
+  end
+
+  describe "when number is not integral" do
+    before { @new_level.number = 5.4 }
+    it { should_not be_valid }
+  end
+
+  describe "when calorie_goal is not present" do
+    before { @new_level.calorie_goal = " " }
+    it { should_not be_valid }
+  end
+
+  describe "when calorie_goal is negative" do
+    before { @new_level.calorie_goal = -1 }
+    it { should_not be_valid }
+  end
+
+  describe "when calorie_goal is not numeric" do
+    before { @new_level.calorie_goal = "five" }
+    it { should_not be_valid }
+  end
+
+  describe "when calorie_goal is not integral" do
+    before { @new_level.calorie_goal = 5.4 }
+    it { should_not be_valid }
+  end
+
+  describe "when activity_goal is not present" do
+    before { @new_level.activity_goal = " " }
+    it { should_not be_valid }
+  end
+
+  describe "when activity_goal is negative" do
+    before { @new_level.activity_goal = -1 }
+    it { should_not be_valid }
+  end
+
+  describe "when activity_goal is not numeric" do
+    before { @new_level.activity_goal = "five" }
+    it { should_not be_valid }
+  end
+
+  describe "when activity_goal is not integral" do
+    before { @new_level.activity_goal = 5.4 }
+    it { should_not be_valid }
+  end
+end
